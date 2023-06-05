6d31b2b9512d0067145c8c0fd4de5800e9ea8b4e
Genia V <evgenia.verhovtseva@yandex.ru>
2023-06-05 15:40:45 +0300

    add: gh-pages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_WEATHER_URL = "http://api.weatherapi.com/v1";
+export const API_WEATHER_URL = "https://api.weatherapi.com/v1";
 export const API_CURRANCY_URL = "https://api.apilayer.com";
 export const apiKey = '95f8d6b22f314330b7d194938232005';
 export const apiCurrancyKey = 'PxqQGkIWp1HanDr41xCk6SQPE6zqDjpS';
